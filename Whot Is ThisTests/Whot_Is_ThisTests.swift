d12690a937c0f727e309865b7e625984eef34386
Mahmud Assamaray <mahmud@Mahmuds-MacBook.local>
2015-08-20 16:33:23 -0400

    Initial Commit

---
+++ Whot Is ThisTests/Whot_Is_ThisTests.swift
@@ -0,0 +1,36 @@
+//
+//  Whot_Is_ThisTests.swift
+//  Whot Is ThisTests
+//
+//  Created by Mahmud Assamaray on 8/20/15.
+//  Copyright (c) 2015 Mahmud Ahmad. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Whot_Is_ThisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
